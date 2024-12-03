e33d0bf7225e3f324180ef9f745208ee82aa16c4
sergey2k161 <you@example.com>
2024-12-03 18:44:49 +0300

    fds

--- DataAccess/INoteRepository.cs
+++ DataAccess/INoteRepository.cs
@@ -3,5 +3,5 @@
 public interface INoteRepository
 {
     Task CreateAsync(Note note, CancellationToken cancellationToken = default);
-    Task<Note?> GetByIdAsync(Guid id, CancellationToken cancellationToken = default);
+    Task<Note?> GetByIdAsync(Guid id);
 }
